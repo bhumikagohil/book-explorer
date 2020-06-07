5b4d57ba96de96a1b580c2c41f7f14c9c05e253b
Bhumika Gohil <bhumikagohil57@gmail.com>
2020-06-07 20:22:10 +0530

    Updates

---
+++ precache-manifest.8db27f33d6f9accbaa1c7e9220ef507e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253ea88bc6a93e1c61264d3d01af9e94",
+    "url": "/book-explorer/index.html"
+  },
+  {
+    "revision": "33f037ca2275e072ae17",
+    "url": "/book-explorer/static/css/main.bb0edcdf.chunk.css"
+  },
+  {
+    "revision": "f9b85859a79ff6a4227d",
+    "url": "/book-explorer/static/js/2.eb4c78f9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/book-explorer/static/js/2.eb4c78f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f037ca2275e072ae17",
+    "url": "/book-explorer/static/js/main.72c1fe56.chunk.js"
+  },
+  {
+    "revision": "1ff765470aed87709319",
+    "url": "/book-explorer/static/js/runtime-main.b4aa1836.js"
+  }
+]);
