1367f04b8046416ba787103c2d0a86ba889199a8
Bhumika Gohil <bhumikagohil57@gmail.com>
2020-11-27 17:39:06 +0530

    final deployment

---
+++ build/precache-manifest.9cce5c3f59229d210ead75b868920b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9b137fb1014d0046b27eb84144a220",
+    "url": "/book-explorer/index.html"
+  },
+  {
+    "revision": "5b9d40e189c2ace97aa9",
+    "url": "/book-explorer/static/css/main.f8056cf7.chunk.css"
+  },
+  {
+    "revision": "db996a259bd69db71cc0",
+    "url": "/book-explorer/static/js/2.3ad492dd.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/book-explorer/static/js/2.3ad492dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b9d40e189c2ace97aa9",
+    "url": "/book-explorer/static/js/main.e93bef4b.chunk.js"
+  },
+  {
+    "revision": "41065ba0e8c9fc5f6bb7",
+    "url": "/book-explorer/static/js/runtime-main.8aaa8c69.js"
+  }
+]);
